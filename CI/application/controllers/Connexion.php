4b4cb6f9aab6de8bcd4da8f65bc705984a6efe52
leblet <bastien.leblet@etu.u-pec.fr>
2021-05-25 17:54:18 +0200

    -

--- CI/application/controllers/Connexion.php
+++ CI/application/controllers/Connexion.php
@@ -1,17 +1,15 @@
 <?php 
 class Connexion extends CI_Controller {
 
-    
     public function index()
 	{
-		
+        $this->load->view('templates/header');
         $this->load->view('connexion');
-        
-
+        $this->load->view('templates/footer');
     }
      
 }
 
-    ?>
+?>
 
 
