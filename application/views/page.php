ef02a91b0771bfdc10b2f9b71a40617229b6b656
leblet <bastien.leblet@etu.u-pec.fr>
2021-05-31 17:22:41 +0200

    _

--- application/views/page.php
+++ application/views/page.php
@@ -1,5 +1,5 @@
-
-<!--
+<?php
+/*
 ###########################################################################
 #                                                                         #
 #                                                                         #
@@ -12,38 +12,44 @@
 #     #####        #####       #####      #####      ######    ######     #
 #                                                                         #
 #                                                                         #
-#                Réalisé par Bastien LEBLET & Kévin METRI                #
+#                Réalisé par Bastien LEBLET & Kévin METRI                 #
 #                                                                         #
 #                                                                         #
 ###########################################################################
-  -->
-
-<?php
-echo "Connecter en temps que  <b>".$_SESSION['login']."</b>";
-if(isset($_GET['deco'])){
-    Securite::Deconnect();
-    header('Location:../connexion/');
-}
-
+*/
 ?>
+<div>
+    <?php
+    echo "Connecter en temps que  <b>".$_SESSION['login']."</b>";
+    if(isset($_GET['deco'])){
+        Securite::Deconnect();
+        header('Location:../connexion/');
+    }
+
+    ?>
+    <form method="get">
+        <input type="hidden" name="deco" value="1">
+        <input type="submit" value="deconnection">
+    </form>
+</div>
 <h1>Selection de sondage </h1>
 
-<div>
-<fieldset class="creer_sondage" >
-    <form method="POST">
-        <label for="titre">Titre <input type="text" name="titre" required></label><br>
-        <label for="lieu">Lieu <input type="text" name="lieu" required></label><br>
-        <label for="descriptif">Descriptif <input type="text" name="descriptif" required></label><br>
-        <label for="date_debut">Date début <input type="date" min="<?php echo date('Y-m-d'); ?>" name="date_debut"  required></label><br>
-        <label for="date_fin">Date fin <input type="date" min="<?php echo date('Y-m-d'); ?>" name="date_fin" required></label><br>
-        <label for="heure_debut">Heure début<input type="time" name="heure_debut" required></label>
-        <label for="heure_fin">Heure fin <input type="time" name="heure_fin" required></label><br>
-        <input type="hidden" name="login" value='<?php echo($_SESSION['login'])?>'class="bouton3" required></label>
-        <input type="submit" value="Créer le sondage" class="bouton3">
-    </form>
-    </fieldset>
-</div>
 
+<fieldset class="creer_sondage" >
+    <div>
+        <form method="POST">
+            <label for="titre">Titre <input type="text" name="titre" required></label><br>
+            <label for="lieu">Lieu <input type="text" name="lieu" required></label><br>
+            <label for="descriptif">Descriptif <input type="text" name="descriptif" required></label><br>
+            <label for="date_debut">Date début <input type="date" min="<?php echo date('Y-m-d'); ?>" name="date_debut"  required></label><br>
+            <label for="date_fin">Date fin <input type="date" min="<?php echo date('Y-m-d'); ?>" name="date_fin" required></label><br>
+            <label for="heure_debut">Heure début<input type="time" name="heure_debut" required></label>
+            <label for="heure_fin">Heure fin <input type="time" name="heure_fin" required></label><br>
+            <input type="hidden" name="login" value='<?php echo($_SESSION['login'])?>'class="bouton3" required></label>
+            <input type="submit" value="Créer le sondage" class="bouton3">
+        </form>
+    </div>
+</fieldset>
     <fieldset class='liste_sondage_user'>
         <?php
         echo "  <legend>Sondage créer  </legend>";
@@ -56,10 +62,5 @@ if(isset($_GET['deco'])){
         }   
         ?>
     </fieldset>
-    
 <br>
 
-<form method="get">
-    <input type="hidden" name="deco" value="1">
-    <input type="submit" value="deconnection">
-</form>
