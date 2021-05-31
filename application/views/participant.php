beec8fe56f7930383a89b5a01d38608b0c30a0ed
metri <kevin.metri@etu.u-pec.fr>
2021-05-31 10:04:48 +0200

    _emploi du temps sondage

--- application/views/participant.php
+++ application/views/participant.php
@@ -1,8 +1,10 @@
-
+<style type="text/css">
+      
+    </style>
 
 <h1> ICI C'EST MA VUE DES PARTICIPANTS </h1>
 
-<?php
+<!-- <?php
  foreach($titres as $titre){
     $titre_hash= hash("ripemd160",$titre->titre);
     
@@ -18,5 +20,33 @@
 
     }
   }
+?> -->
+
+<table>
+<?php
+    $jour = array(null, "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
+    $rdv["Dimanche"]["16:30"] = "";
+    $rdv["Lundi"]["9"] = "";
+    echo "<tr><th>Heure</th>";
+    for($x = 1; $x < 8; $x++)
+        echo "<th>".$jour[$x]."</th>";
+    echo "</tr>";
+    for($j = 0; $j < 24; $j += 0.5) {
+        echo "<tr>";
+        for($i = 0; $i < 7; $i++) {
+            if($i == 0) {
+                $heure = str_replace(".5", ":30", $j);
+                if(substr($heure,-3,3) != ":30")
+                    echo "<td class=\"time\" rowspan=\"2\">".$heure."h</td>";
+            }
+            echo "<td>";
+            if(isset($rdv[$jour[$i+1]][$heure])) {
+                echo $rdv[$jour[$i+1]][$heure];
+            }
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
 ?>
+</table>
 
